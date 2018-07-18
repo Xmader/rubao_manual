1495dae88edc43e810df03bfb1eec86373ba5d5e
Xmader <xmader@outlook.com>
2018-07-18 17:51:54 +0800

    增加内容

--- copy.js
+++ copy.js
@@ -13,7 +13,7 @@ if (is_electron_app) {
         console.log(text)
         clipboard.writeText(text)
 
-        var messages = [`防民之口，甚于防川。`,"敢同恶鬼争高下，不向霸王让寸分。","年青人呐，都是靡靡之音呐，都是轻歌曼舞啊。我跟你说长大了都没出息。这个呀，就是得豪迈！"]
+        var messages = [`防民之口，甚于防川。`,"别看你今天闹得欢，小心啊，今后拉清单","敢同恶鬼争高下，不向霸王让寸分。","年青人呐，都是靡靡之音呐，都是轻歌曼舞啊。我跟你说长大了都没出息。这个呀，就是得豪迈！"]
 
         dialog.showMessageBox({
             type: "info",
