41aaf777d5c8adf85457dcd7848214e91a664b26
Xmader <xmader@outlook.com>
2018-07-18 20:52:26 +0800

    更新内容

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 > 庆丰不识字，何故乱宽衣。
 
+**[进入 乳包指南](https://xmader.github.io/rubao_manual/)**
+
 ## 开源许可证
 
 MIT
--- index.js
+++ index.js
@@ -11,16 +11,16 @@ const getArgs = () => {
 
 const init_modal = (key, a) => {
     var item = json[key][a]
-    var quote = item["quote"]?`<h3>“${item["quote"]}”</h3><br />`:""
+    var quote = item["quote"] ? `<h3>“${item["quote"]}”</h3><br />` : ""
     $("#m_title").text(item["title"])
-    $("#m_body").html(quote+"<p>"+item["content"].replace(/\n/g, "</p><p>"))
-    $("#m_unformatted_body")[0].value = item["content"].replace(/<(S*?)[^>]*>.*?|<.*? \/>/g,"")
+    $("#m_body").html(quote + "<p>" + item["content"].replace(/\n/g, "</p><p>"))
+    $("#m_unformatted_body")[0].value = item["content"].replace(/<(S*?)[^>]*>.*?|<.*? \/>/g, "")
 }
 
 
 var is_electron_app = navigator.userAgent.indexOf("Electron") > -1
 const is_Firefox = navigator.userAgent.indexOf("Firefox") > -1;
-const is_Chrome = navigator.userAgent.indexOf("Chrome") > -1;
+const is_Chrome = (navigator.userAgent.indexOf("Chrome") > -1) && window.chrome;
 
 
 const json = rubao_json
@@ -45,8 +45,8 @@ for (var i = 0; i < keys.length; i++) {
             audio = `<a href="${json["url"]}${item["audio"]}.mp3" target="_blank" class="download_music" download><i class="fa fa-download" aria-hidden="true"></i></a>
         <audio class="audio${is_Firefox ? "_Firefox" : ""}" src="${json["url"]}${item["audio"]}.mp3" controls></audio>`
         }
-        
-        item_html += `<li class="list-group-item grey">
+
+        item_html += `<li class="list-group-item grey chang ${is_Chrome || is_electron_app ? "chang_chrome" : " "}">
         <a data-toggle="modal" href="#modal" data-target="#modal" onclick="init_modal('${key}','${a}');">${item["title"]}</a>
         ${audio}
         </li>`
