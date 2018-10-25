d1cf01c60510501cb41059e59b24c011bc591256
Xmader <xmader@outlook.com>
2018-10-25 22:15:24 +0800

    vue init

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const getArgs = () => {
-    var args = {};
-    var match = null;
-    var search = decodeURIComponent(location.search.substring(1));
-    var reg = /(?:([^&]+)=([^&]+))/g;
-    while ((match = reg.exec(search)) !== null) {
-        args[match[1]] = match[2];
-    }
-    return args;
-}
 
 // 网页内全屏视频
 
